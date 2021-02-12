3a30452f932ecfe4eb8061d4e328745e395215d3
Luke Staib <ljstaib@gmail.com>
2021-02-11 19:11:04 -0500

    Created psuedocode for the secure file uploader/ingest, text NLP analysis, and newsfeed ingest. Also, I made an empty testing file to implement actions.

---
+++ newsanalyzer_test.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+# ========================================================================
+# Luke Staib ljstaib@bu.edu 
+# Copyright @2021, for EC500: Software Engineering
+# News Analyzer Testing
+# ========================================================================
+
+# ========================================================================
+# Import newsanalyzer.py
+# ========================================================================
+
+from newsanalyzer import *
+
+# ========================================================================
+# File Uploader/Ingest Testing
+# ========================================================================
+
+def test_UploadFiles():
+
+def test_DisplayUploadStatus():
+
+def test_RenderProgressBar():
+
+def test_UploadError():
+
+def test_CancelUpload():
+
+def test_FileDelete():	
+
+def test_FileEditName():
+
+def test_OrganizeFileList():
+
+# ========================================================================
+# Text NLP Analysis Testing
+# ========================================================================	
+
+def ConvertFilesToText():
+
+def CreateKeywords():
+
+def ObtainArticles():
+
+def AssessData():
+
+def SaveSentiment():
+
+def EditSentiment():
+
+def Translate():
+
+# ========================================================================
+# Newsfeed Ingest Testing
+# ========================================================================
+
+def DiscoverContent():
+
+def DisplayContent():
+
+def OrganizeContent():
+
+def ReadLater():
+
+# ========================================================================
+# Logging and Diagnostics Testing
+# ========================================================================
+
+def SendLogReport():
+
+def Diagnostics():
