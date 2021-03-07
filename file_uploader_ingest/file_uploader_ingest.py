9c8454e67296c1244b3cc3491829d07017f387e9
Luke Staib <ljstaib@gmail.com>
2021-03-07 18:35:56 -0500

    Fixing testing

--- file_uploader_ingest/file_uploader_ingest.py
+++ file_uploader_ingest/file_uploader_ingest.py
@@ -12,7 +12,6 @@
 # ========================================================================
 
 #Import Data
-from db import *
 
 #Import libraries
 import cProfile #CPU
@@ -34,7 +33,7 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'docx'}
 import sys
 sys.path.append('../')
 from db import *
-sys.path.append('./NLP_analysis')
+sys.path.append('../NLP_analysis')
 from NLP_analysis import ConvertFileToText
 
 # files = ["Sample.txt", "DONOTREAD.docx", "WhiteHouseBriefing.pdf"] #Sample list
